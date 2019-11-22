a67818624db3d9360eb917667daed2a8cecef9a5
Chawatvish Worrapoj <chawatvish.wo@gmail.com>
2019-11-22 15:32:22 +0700

    init project

---
+++ router.php
@@ -0,0 +1,48 @@
+<?php
+    $requestURI = $_SERVER["REQUEST_URI"];
+    $filePath = __DIR__  . $requestURI;
+
+    if(file_exists($filePath) && is_file($filePath)){
+      $fileInfo = pathinfo($filePath);
+      if ($fileInfo["extension"] != "html"){
+        return false;
+      }
+    }
+    $session = isset($_COOKIE["authentication"])?true:false;
+    //echo "what?";
+    if($session){
+      if($requestURI == '/deposit') {
+      	include 'view/deposit.html';
+      }
+      elseif($requestURI == '/pay') {
+      	include 'view/pay.html';
+      }
+      elseif($requestURI == '/pay/electric') {
+        include 'view/pay/electric.html';
+      }
+      elseif($requestURI == '/pay/phone') {
+        include 'view/pay/phone.html';
+      }
+      elseif($requestURI == '/pay/water') {
+        include 'view/pay/water.html';
+      }
+      elseif($requestURI == '/transfer') {
+      	include 'view/transfer.html';
+      }
+      elseif($requestURI == '/withdrawal') {
+      	include 'view/withdrawal.html';
+      }
+      else{
+        if($requestURI != "/main"){
+          header("Location: view/main.html");
+        }
+        include 'view/main.html';
+      }
+    }
+    else {
+      if($requestURI != "/"){
+        header("Location: view/login.html");
+      }
+      include 'view/login.html';
+    }
+?>
