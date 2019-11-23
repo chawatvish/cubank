dfb2abda0ed50a986f10314b42f751301c863f2e
Chawatvish Worrapoj <chawatvish.wo@gmail.com>
2019-11-23 20:13:31 +0700

    Fixed issue on PRD testing.

--- test/transfer/TransferTestPRD.php
+++ test/transfer/TransferTestPRD.php
@@ -1,40 +1,46 @@
 <?php
 
-include_once __DIR__ . "/../src/transfer/Transfer.php";
+require_once __DIR__ . "./../../src/transfer/transfer.php";
 
 use Operation\Transfer;
 use PHPUnit\Framework\TestCase;
 
 class TransferTest extends TestCase
 {
+    private $transferService;
+
+    protected function _before() {
+        $this->transferService = new Transfer();
+    }
+
     public function testTC001()
-    {
-        $transfer = new Transfer('3333333001', 'TEST 001');
-        $result = $transfer->doTransfer('333333300g', 5000);
+    {   
+        $this->_before();
+        $result = $this->transferService->doTransfer('9999999999','333333300g', '500');
         $this->assertTrue($result['isError']);
         $this->assertEquals("หมายเลขบัญชีไม่ถูกต้อง", $result["message"]);
     }
 
     public function testTC002()
     {
-        $transfer = new Transfer('3333333001', 'TEST 001');
-        $result = $transfer->doTransfer('123456789', 5000);
+        $this->_before();
+        $result = $this->transferService->doTransfer('9999999999', '123456789', '5000');
         $this->assertTrue($result['isError']);
         $this->assertEquals("หมายเลขบัญชีไม่ถูกต้อง", $result["message"]);
     }
 
     public function testTC003()
     {
-        $transfer = new Transfer('3333333001', 'TEST 001');
-        $result = $transfer->doTransfer('123456789', 5000);
+        $this->_before();
+        $result = $this->transferService->doTransfer('9999999999', '123456789', '5000');
         $this->assertTrue($result['isError']);
         $this->assertEquals("หมายเลขบัญชีไม่ถูกต้อง", $result["message"]);
     }
     
     public function testTC004()
     {
-        $transfer = new Transfer('3333333001', 'TEST 001');
-        $result = $transfer->doTransfer('3333333005', 50000);
+        $this->_before();
+        $result = $this->transferService->doTransfer('9999999999','9999900001', '500000000');
         $this->assertTrue($result['isError']);
         $this->assertEquals("ยอดเงินไม่เพียงพอ", $result["message"]);
     }
